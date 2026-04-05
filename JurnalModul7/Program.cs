01316b2c3aa302a78862d1d3ede8b35ce0919ac1
AdityaAdnyana <hiddenaditya4@gmail.com>
2026-04-05 19:19:19 +0700

    feat: menambahkan class TeamMembers103082400005

--- JurnalModul7/Program.cs
+++ JurnalModul7/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     public static void Main()
     {
-        DataMahasiswa103082400005.ReadJSON();
+        //DataMahasiswa103082400005.ReadJSON();
+        TeamMembers103082400005.readJSON();
     }
 }
---
+++ JurnalModul7/TeamMembers103082400005.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JurnalModul7
+{
+    internal class TeamMembers103082400005
+    {
+
+        public class TeamMembers
+        {
+            public class Member
+            {
+                [JsonPropertyName("firstName")]
+                public string FirstName { get; set; }
+                [JsonPropertyName("lastName")]
+                public string LastName { get; set; }
+                [JsonPropertyName("gender")]
+                public string Gender { get; set; }
+                [JsonPropertyName("age")]
+                public int Age {  get; set; }
+                [JsonPropertyName("nim")]
+                public string Nim {  get; set; }
+
+            }
+
+            [JsonPropertyName("members")]
+            public List<Member> Members { get; set; }
+
+        }
+
+        public static void readJSON()
+        {
+            string strtingJSON = File.ReadAllText("../../../jurnal7_2_103082400005.json");
+
+            try
+            {
+                TeamMembers teamMembers = JsonSerializer.Deserialize<TeamMembers>(strtingJSON);
+
+                Console.WriteLine("Team member list:");
+                foreach (var member in teamMembers.Members)
+                {
+                    Console.WriteLine($"{member.Nim} {member.FirstName} {member.LastName} ({member.Age} {member.Gender})");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Tidak dapat memuat JSON: {ex.Message}");
+            }
+        }
+    }
+}
